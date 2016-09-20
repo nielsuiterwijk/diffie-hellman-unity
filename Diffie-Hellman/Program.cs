0d6d8aa9a039aa7d492b5b3646cf1488faa0fdf8
Niels.Uiterwijk <nielsuiterwijk@gmail.com>
2016-09-20 21:38:58 +0200

    Added BigInteger, taken from mono.

---
+++ Diffie-Hellman/Program.cs
@@ -0,0 +1,19 @@
+﻿using Numerics;
+using System;
+using System.Security.Cryptography;
+
+namespace Diffie_Hellman
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BigInteger secretPhrase = BigInteger.GenerateRandom(1024);
+
+			BigInteger publicModulo = BigInteger.GenerateRandom(1024);
+
+			Console.ReadKey();
+
+		}
+	}
+}
