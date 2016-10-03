3508af9065852e5fb6d6f9a31214a205f17a17cd
Niels.Uiterwijk <nielsuiterwijk@gmail.com>
2016-10-03 21:18:10 +0200

    Added public 2048 bit prime

--- Diffie-Hellman/Program.cs
+++ Diffie-Hellman/Program.cs
@@ -8,7 +8,20 @@ namespace Diffie_Hellman
 {
 	class Program
 	{
-		static BigInteger publicModulo = BigInteger.GenerateRandom(256);
+		internal static readonly uint[] Prime2048 = new uint[64] {  0xFFFFFFFF, 0xFFFFFFFF, 0xC90FDAA2, 0x2168C234, 0xC4C6628B, 0x80DC1CD1,
+				0x29024E08, 0x8A67CC74, 0x020BBEA6, 0x3B139B22, 0x514A0879, 0x8E3404DD,
+				0xEF9519B3, 0xCD3A431B, 0x302B0A6D, 0xF25F1437, 0x4FE1356D, 0x6D51C245,
+				0xE485B576, 0x625E7EC6, 0xF44C42E9, 0xA637ED6B, 0x0BFF5CB6, 0xF406B7ED,
+				0xEE386BFB, 0x5A899FA5, 0xAE9F2411, 0x7C4B1FE6, 0x49286651, 0xECE45B3D,
+				0xC2007CB8, 0xA163BF05, 0x98DA4836, 0x1C55D39A, 0x69163FA8, 0xFD24CF5F,
+				0x83655D23, 0xDCA3AD96, 0x1C62F356, 0x208552BB, 0x9ED52907, 0x7096966D,
+				0x670C354E, 0x4ABC9804, 0xF1746C08, 0xCA18217C, 0x32905E46, 0x2E36CE3B,
+				0xE39E772C, 0x180E8603, 0x9B2783A2, 0xEC07A28F, 0xB5C55DF0, 0x6F4C52C9,
+				0xDE2BCBF6, 0x95581718, 0x3995497C, 0xEA956AE5, 0x15D22618, 0x98FA0510,
+				0x15728E5A, 0x8AACAA68, 0xFFFFFFFF, 0xFFFFFFFF
+																 };
+
+		static BigInteger publicModulo = new BigInteger(Prime2048);
 		static BigInteger publicBase = 9;
 
 		static BigInteger aliceSecret =  BigInteger.GenerateRandom(128);
@@ -17,6 +30,10 @@ namespace Diffie_Hellman
 
 		static void Main(string[] args)
 		{
+			bool result = publicModulo.IsProbablePrime(ushort.MaxValue / 10);
+			Console.WriteLine(publicModulo.ToString() + " is a " + (result ? "prime" : "not a prime!"));
+
+
 			Console.WriteLine("Alice secret: " + aliceSecret.ToString());
 			Console.WriteLine("Bob secret:   " + bobSecret.ToString());
 
@@ -31,7 +48,13 @@ namespace Diffie_Hellman
 			bool equal = aliceSharedSecret == bobSharedSecret;
 			Console.WriteLine("The shared secret is equal: " + equal.ToString());
 
-			AESProfiler.Run(aliceSharedSecret);
+			byte[] hashedSharedKey = SHA256.Create().ComputeHash(aliceSharedSecret.GetBytes());
+
+			byte[] sharedShortKey = new byte[256 / 8];
+
+			Array.Copy(hashedSharedKey, sharedShortKey, 256 / 8);
+
+			AESProfiler.Run(new BigInteger(sharedShortKey));
 
 			Console.ReadKey();
 
---
+++ Diffie-Hellman/Tests/PrimeTest.cs
@@ -0,0 +1,105 @@
+﻿using Numerics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Diffie_Hellman.Tests
+{
+	// Miller - Rabin primality test as an extension method on the BigInteger type.
+// Based on the Ruby implementation on this page.
+	public static class BigIntegerExtensions
+	{
+		public static int progress = 0;
+		public static bool IsProbablePrime(this BigInteger source, int certainty)
+		{
+			if (source == 2 || source == 3)
+			{
+				return true;
+			}
+
+			if (source < 2 || source % 2 == 0)
+			{
+				return false;
+			}
+
+			BigInteger d = source - 1;
+			int s = 0;
+
+			while (d % 2 == 0)
+			{
+				d /= 2;
+				s += 1;
+			}
+
+			// There is no built-in method for generating random BigInteger values.
+			// Instead, random BigIntegers are constructed from randomly generated
+			// byte arrays of the same length as the source.
+			RandomNumberGenerator rng = RandomNumberGenerator.Create();
+			byte[] bytes = new byte[source.GetBytes().LongLength];
+			BigInteger a;
+
+			int steps = certainty / 1000;
+			double percent = 0;
+
+			Stopwatch timer = new Stopwatch();
+
+			timer.Start();
+
+			for (int i = 0; i < certainty; i++)
+			{
+				if (i != 0 && i % steps == 0)
+				{
+					percent++;
+
+					Console.WriteLine((percent / 10.0d) + "% took " + timer.ElapsedMilliseconds + "ms so far!");
+
+				}
+
+
+
+				do
+				{
+					// This may raise an exception in Mono 2.10.8 and earlier.
+					// http://bugzilla.xamarin.com/show_bug.cgi?id=2761
+					rng.GetBytes(bytes);
+					a = new BigInteger(bytes);
+				}
+				while (a < 2 || a >= source - 2);
+
+				BigInteger x = a.ModPow(d, source);
+
+				if (x == 1 || x == source - 1)
+				{
+					continue;
+				}
+
+				for (int r = 1; r < s; r++)
+				{
+					x = x.ModPow(2, source);
+
+					if (x == 1)
+					{
+						return false;
+					}
+
+					if (x == source - 1)
+					{
+						break;
+					}
+				}
+
+				if (x != source - 1)
+				{
+					return false;
+				}
+			}
+
+
+
+			return true;
+		}
+	}
+}
